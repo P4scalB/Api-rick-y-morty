5984aaa6e98ad2aa5e095be7624a2f89334d2de3
P4scal <83776555+P4scalB@users.noreply.github.com>
2021-07-18 20:57:44 -0500

    proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, {Fragment} from "react";
-
+import React, {Fragment} from 'react'
+import Header from './components/Header'
+import Characters from './components/Characters'
+import './main.sass'
+import './components/Header.css'
+import './components/Characters.css'
 
 const App = () => {
     return ( 
         <Fragment>
-            <h1>Hola mundo</h1>
+            <Header
+                title="Rick and Morty API"
+            />
+            <Characters />
         </Fragment>
      );
 }
